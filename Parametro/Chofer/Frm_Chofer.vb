e88d080b32ba3053542edb2d00484e8d7497b1d2
bacotich <bacotich_daniel@informaticos.com>
2014-10-18 17:13:22 -0400

    prueba

--- Parametro/Chofer/Frm_Chofer.vb
+++ Parametro/Chofer/Frm_Chofer.vb
@@ -9,4 +9,7 @@
         sender.SelectAll()
     End Sub
 
+    Private Sub btn_cancelar_Click(sender As Object, e As EventArgs) Handles btn_cancelar.Click
+        Me.Close()
+    End Sub
 End Class
